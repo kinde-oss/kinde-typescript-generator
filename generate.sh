70b3d5cae2a68a4395ae73478604ef7ab73ea1c1
maheen-zahid <maheen.zahid@devsinc.com>
2023-06-04 23:12:31 +0500

    updated generation script and configuration file
    - ensured that generation script structures resulting SDK correctly
    - git ignoring kinde-mgmt-api-specs.yaml
    - removed extraneous clean.sh script

--- generate.sh
+++ generate.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-# Get latest API spec
-rm -f ./kinde-mgmt-api-specs.yaml
-curl https://kinde.com/api/kinde-mgmt-api-specs.yaml -o kinde-mgmt-api-specs.yaml
+# fetching latest open-api specification
+rm -rf ./generated-sdk && rm -f ./kinde-mgmt-api-specs.yaml
+curl https://kinde.com/api/kinde-mgmt-api-specs.yaml \
+  -o kinde-mgmt-api-specs.yaml
 
-# Generate SDK
-docker run --rm -v $(PWD):/local   \
+# generating sdk
+docker run --rm -v $PWD:/local \
     openapitools/openapi-generator-cli generate \
-    -i /local/kinde-mgmt-api-specs.yaml -c /local/config.yaml -g typescript-fetch \
+    -i /local/kinde-mgmt-api-specs.yaml \
+    -c /local/config.yaml -g typescript-fetch \
     -o /local/generated-sdk
 
-# Clean up API files from SDK
-rm -rf ./generated-sdk/.openapi-generator ./generated-sdk/.openapi-generator-ignore
+# making sdk-version.sh script executable
+chmod +x generated-sdk/sdk-version.sh
+
+# moving apis, models, runtime.ts to lib directory
+mkdir -p ./generated-sdk/lib 
+mv ./generated-sdk/runtime.ts ./generated-sdk/lib
+mv ./generated-sdk/models ./generated-sdk/lib
+mv ./generated-sdk/apis ./generated-sdk/lib 
+
+# clean up api files from sdk
+rm -rf ./generated-sdk/.openapi-generator 
+rm -rf ./generated-sdk/.openapi-generator-ignore
